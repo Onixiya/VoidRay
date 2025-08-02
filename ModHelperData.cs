95e02859591ac4bfa08a426f1631d0cd44621a74
silentstorm <29950414+Onixiya@users.noreply.github.com>
2025-08-02 10:06:12 +1000

    Update ModHelperData

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,7 +1,7 @@
 namespace VoidRay{
     public static class ModHelperData{
         public const string WorksOnVersion="49.3";
-        public const string Version="1.0.2";
+        public const string Version="1.0.3";
         public const string Name="Void Ray";
         public const string Description="Adds the Void Ray from Starcraft 2 as a military tower into BTD6";
         public const string RepoOwner="Onixiya";
